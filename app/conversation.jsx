5fe3ff457c55f6ae57644b4d3c748504f4daabed
m-abdelghani02 <medj_abdelghani@outlook.com>
2024-07-21 19:59:30 +0100

    receiving messages works

--- app/conversation.jsx
+++ app/conversation.jsx
@@ -11,40 +11,38 @@ import { LinearGradient } from "expo-linear-gradient";
 import Constants from "expo-constants";
 import { StatusBar } from "expo-status-bar";
 import ChatHeader from "../components/ChatHeader";
-import { useLocalSearchParams, useRouter } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
 import MessageList from "../components/MessageList";
 import { Ionicons } from "@expo/vector-icons";
-import uuid from "react-native-uuid"; // Import uuid
-import * as ImagePicker from 'expo-image-picker'
-import {getConversationMessages} from '../services/messageService';
-import { sendMessageHandler } from '../handlers/sendMessageHandler.js'; 
+import uuid from "react-native-uuid";
+import * as ImagePicker from 'expo-image-picker';
+import { getConversationMessages } from '../services/messageService';
+import { sendMessageHandler } from '../handlers/sendMessageHandler';
 import { getConversationById } from "../services/conversationService";
 import { authService } from "../services/authService";
+import * as socketService from '../services/socketService';
 
 const Conversation = () => {
   const item = useLocalSearchParams();
   const conversationId = Object.values(item).join('');
-  const router = useRouter();
   const [messageText, setMessageText] = useState("");
-  const [convMessages, setConvMessages] = useState([])
   const [messages, setMessages] = useState([]);
   const [convDetails, setConvDetails] = useState({});
   const [user2_id, setUser2_id] = useState('');
-  //const convMessages = getConversationMessages(item.conversationId);
+
   useEffect(() => {
-    console.log('Conversation ID:', conversationId);
     const fetchMessages = async () => {
       try {
         const conversationMessages = await getConversationMessages(conversationId);
         console.log('Fetched Messages:', conversationMessages);
 
-        // Transforming the messages
         const transformedMessages = transformMessages(conversationMessages);
         setMessages(transformedMessages);
       } catch (error) {
         console.error('Error fetching messages:', error);
       }
     };
+
     const fetchConvDetails = async () => {
       try {
         const convDetails = await getConversationById(conversationId);
@@ -53,48 +51,66 @@ const Conversation = () => {
       } catch (error) {
         console.log(error);
       }
-    }
+    };
+
+    const handleMessageReceived = (message) => {
+      console.log('Received message:', message);
+      receiveMessage(message);
+    };
+
+    const initializeSocket = async () => {
+      if (!socketService.socket) {
+        console.log('Attempting to connect socket...');
+        await socketService.connect();
+      } else {
+        console.log('Socket already connected');
+      }
+
+      if (socketService.socket) {
+        socketService.socket.on('messageSent', handleMessageReceived);
+        console.log('Listener for messageSent added');
+      }
+    };
+
+    initializeSocket();
+
     fetchConvDetails();
     fetchMessages();
-  }, [conversationId]);
-  
-  useEffect(() => {
-    console.log('Conversation Messages State:', convMessages);
-    console.log('Conversation Details State:', convDetails);
-    console.log('Conversation Recipient State:', user2_id);
-  }, [convMessages, convDetails, user2_id]);
 
-  
-  
+    return () => {
+      if (socketService.socket) {
+        socketService.socket.off('messageSent', handleMessageReceived);
+        console.log('Listener for messageSent removed');
+      }
+    };
+  }, [conversationId]);
+
   const sendMessage = async () => {
     const newMessageId = uuid.v4();
     const newMessage = {
       id: newMessageId,
       text: messageText,
       type: "sent",
-      time: formatTimestamp(new Date()), // Current time
+      time: formatTimestamp(new Date()),
       isRead: false,
-      status: "pending", // Mark as pending initially
+      status: "pending",
     };
 
-    // Optimistically update the UI
     setMessages((prevMessages) => [...prevMessages, newMessage]);
     setMessageText("");
 
-    const sentMessageData = { conversationId, content: messageText, recipient_id: user2_id };
+    const sentMessageData = { message_id: newMessageId, conversation_id: conversationId, content: messageText, recipient_id: user2_id };
     console.log("Sending from conversation", sentMessageData);
 
     const success = await sendMessageHandler(sentMessageData);
 
     if (success) {
-      // Update the message status to sent
       setMessages((prevMessages) =>
         prevMessages.map((msg) =>
           msg.id === newMessageId ? { ...msg, status: "sent" } : msg
         )
       );
     } else {
-      // Update the message status to failed
       setMessages((prevMessages) =>
         prevMessages.map((msg) =>
           msg.id === newMessageId ? { ...msg, status: "failed" } : msg
@@ -103,42 +119,46 @@ const Conversation = () => {
       console.error('Error sending message');
     }
   };
-  
 
+  const receiveMessage = (message) => {
+    const transformedMessage = {
+      id: message.message_id,
+      text: message.content,
+      type: 'received',
+      time: formatTimestamp(new Date()), // Adjust if timestamp is provided
+      isRead: false,
+    };
+    setMessages((prevMessages) => [...prevMessages, transformedMessage]);
+  };
 
   const transformMessages = (messages) => {
     return messages.map((message) => ({
-      id: message.message_id, // Generating a new unique ID
-      text: message.content, // Mapping the 'content' field
-      type: message.sender_id === authService.getUser().id ? 'sent' : 'received', // Determining message type based on HARDCODED sender_id
-      time: formatTimestamp(message.timestamp), // Formatting the timestamp
-      isRead: false, // Assuming all messages are unread initially
+      id: message.message_id,
+      text: message.content,
+      type: message.sender_id === authService.getUser().id ? 'sent' : 'received',
+      time: formatTimestamp(message.timestamp),
+      isRead: false,
     }));
   };
-  
-  // Utility function to format timestamp
+
   const formatTimestamp = (timestamp) => {
-    // Convert timestamp to HH:MM format (for example)
     const date = new Date(timestamp);
     return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
   };
-
-
-
 
   const openPicker = async (selectType) => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: selectType === 'image' ? ImagePicker.MediaTypeOptions.Images : ImagePicker.MediaTypeOptions.Videos,
       aspect: [4, 3],
       quality: 1,
-    })
+    });
     console.log(result);
-    if(!result.canceled){
+    if (!result.canceled) {
       console.log('Success');
       setSelectedImage(result.assets[0].uri);
-    } 
-  }
-  
+    }
+  };
+
   return (
     <View className="flex-1">
       <StatusBar style="light" />
@@ -146,7 +166,7 @@ const Conversation = () => {
         colors={["#100025", "rgba(1, 0, 2, 1)"]}
         style={styles.background}
       />
-      <ChatHeader user={item} router={router} />
+      <ChatHeader user={item} />
       <View className="bg-[#141414] w-full h-28 "></View>
 
       <View className="flex-1 justify-between overflow-visible">
@@ -170,9 +190,9 @@ const Conversation = () => {
                 />
               </View>
               {messageText === "" && (
-                <TouchableOpacity 
+                <TouchableOpacity
                   className="pr-4"
-                  onPress={() => {openPicker('image')}}>
+                  onPress={() => { openPicker('image') }}>
                   <Ionicons name="camera-outline" size={24} color="white" />
                 </TouchableOpacity>
               )}
@@ -189,6 +209,7 @@ const Conversation = () => {
     </View>
   );
 };
+
 const height = Dimensions.get("window").height;
 const styles = StyleSheet.create({
   background: {
@@ -197,24 +218,6 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     height: height + Constants.statusBarHeight + 1,
-  },
-  content: {
-    // Center content
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#fff",
-  },
-  picker: {
-    color: "white",
-  },
-  pickerItem: {
-    color: "white", 
-    backgroundColor: "#0F0028", 
   },
 });
 
